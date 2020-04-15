29ebf28be1c124599b0f0f069cf3efab6b2b055f
Kent McKinney <kent.p.mckinney@gmail.com>
2020-04-14 19:42:59 -0700

    repurpose for airbnb clone

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import Tabs from './Tabs';
+import './App.css';
 
 function App() {
   const name = "Thato";
   const name2 = "Haley";
   return (
     <React.Fragment>
-      <Tabs />
+      <main class="parent grid">
+        <div class="div1">Places to Stay </div>
+        <div class="div2">Location</div>
+        <div class="div3">Online Experiences</div>
+        <div class="div4">Monthly Stays</div>
+        <div class="div5">Frontline Stays</div>
+        <div class="div6">We may be apart, but we'll get through<br />this together.</div>
+      </main>
     </React.Fragment>
   )
 }
